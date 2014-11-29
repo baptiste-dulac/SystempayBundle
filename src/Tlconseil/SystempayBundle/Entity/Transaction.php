a5cc0e95a700dff1776f275539bc1d180aaa5f15
Baptiste <streamdemon@gmail.com>
2014-11-29 20:11:03 +0000

    Create a first version with payment creation

---
+++ src/Tlconseil/SystempayBundle/Entity/Transaction.php
@@ -0,0 +1,217 @@
+<?php
+
+namespace Tlconseil\SystempayBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Transaction
+ *
+ * @ORM\Table()
+ * @ORM\Entity()
+ */
+class Transaction
+{
+    /**
+     * @var
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     * @ORM\Column(name="status_code", type="integer")
+     */
+    private $statusCode;
+
+    /**
+     * @var int
+     * @ORM\Column(name="amount", type="integer")
+     */
+    private $amount;
+
+    /**
+     * @var int
+     * @ORM\Column(name="currency", type="integer")
+     */
+    private $currency;
+
+    /**
+     * @var \DateTime
+     * @ORM\Column(name="created_at", type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime
+     * @ORM\Column(name="updated_at", type="datetime")
+     */
+    private $updatedAt;
+
+    /**
+     * @var string
+     * @ORM\Column(name="log_response", type="text", nullable=true)
+     */
+    private $logResponse;
+
+    /**
+     * @var bool
+     * @ORM\Column(name="paid", type="boolean")
+     */
+    private $paid;
+
+    /**
+     * @var bool
+     * @ORM\Column(name="refunded", type="boolean")
+     */
+    private $refunded;
+
+    /**
+     * @return int
+     */
+    public function getAmount()
+    {
+        return $this->amount;
+    }
+
+    /**
+     * @param int $amount
+     */
+    public function setAmount($amount)
+    {
+        $this->amount = $amount;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param \DateTime $createdAt
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCurrency()
+    {
+        return $this->currency;
+    }
+
+    /**
+     * @param int $currency
+     */
+    public function setCurrency($currency)
+    {
+        $this->currency = $currency;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLogResponse()
+    {
+        return $this->logResponse;
+    }
+
+    /**
+     * @param string $logResponse
+     */
+    public function setLogResponse($logResponse)
+    {
+        $this->logResponse = $logResponse;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isPaid()
+    {
+        return $this->paid;
+    }
+
+    /**
+     * @param boolean $paid
+     */
+    public function setPaid($paid)
+    {
+        $this->paid = $paid;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isRefunded()
+    {
+        return $this->refunded;
+    }
+
+    /**
+     * @param boolean $refunded
+     */
+    public function setRefunded($refunded)
+    {
+        $this->refunded = $refunded;
+    }
+
+    /**
+     * @return int
+     */
+    public function getStatusCode()
+    {
+        return $this->statusCode;
+    }
+
+    /**
+     * @param int $statusCode
+     */
+    public function setStatusCode($statusCode)
+    {
+        $this->statusCode = $statusCode;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @param \DateTime $updatedAt
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+    }
+
+
+
+}
---
+++ src/Tlconseil/SystempayBundle/TlconseilSystempayBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Tlconseil\SystempayBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TlconseilSystempayBundle extends Bundle
+{
+}
